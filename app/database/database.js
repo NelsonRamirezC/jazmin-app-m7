2d58264272331c65686fbcd92732fa538b31d000
Nelson Ramirez <xhyskoax@gmail.com>
2023-07-03 11:40:08 -0400

    Cambios datos conexión database

--- app/database/database.js
+++ app/database/database.js
@@ -1,30 +1,17 @@
-import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
 dotenv.config();
 
 let database, user, password, host;
 
-if (process.env.NODE_ENV == "production") {
-    database = process.env.DB_DATABASE ;
-    user = process.env.DB_USER;
-    password = process.env.DB_PASSWORD;
-    host = process.env.DB_HOST;
-}else {
-    database ="db_bootcamp";
-    user = "postgres";
-    password ="123456";
-    host ="localhost";
-} 
-console.log(host);
+database = process.env.DB_DATABASE;
+user = process.env.DB_USER;
+password = process.env.DB_PASSWORD;
+host = process.env.DB_HOST;
 
-const sequelize = new Sequelize(
-    database,
-    user,
-    password,
-    {
-        host: host,
-        dialect: "postgres",
-    }
-);
+const sequelize = new Sequelize(database, user, password, {
+    host: host,
+    dialect: "postgres",
+});
 
 export default sequelize;
